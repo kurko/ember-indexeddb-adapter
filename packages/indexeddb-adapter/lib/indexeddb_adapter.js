cf8f5b9cad11315c2a54a259614acb655dd7476e
Alexandre de Oliveira <chavedomundo@gmail.com>
2014-01-04 05:23:44 -0200

    Adds barebone methods to adapter

--- packages/indexeddb-adapter/lib/indexeddb_adapter.js
+++ packages/indexeddb-adapter/lib/indexeddb_adapter.js
@@ -3,4 +3,48 @@
 'use strict';
 
 DS.IndexedDBAdapter = DS.Adapter.extend({
+  /**
+    @method find
+    @param {DS.Model} type
+    @param {Object|String|Integer|null} id
+    */
+  find: function (store, type, id) {
+    var record = Ember.A();
+
+    return Ember.RSVP.resolve(record);
+  },
+
+  findMany: function (store, type, ids) {
+    var records = Ember.A();
+
+    return Ember.RSVP.resolve(record);
+  },
+
+  findQuery: function (store, type, query, recordArray) {
+    var records = Ember.A();
+
+    return Ember.RSVP.resolve(records);
+  },
+
+  findAll: function (store, type) {
+    var records = Ember.A();
+
+    return Ember.RSVP.resolve(records);
+  },
+
+  createRecord: function (store, type, record) {
+    return Ember.RSVP.resolve(record);
+  },
+
+  updateRecord: function (store, type, record) {
+    return Ember.RSVP.resolve();
+  },
+
+  deleteRecord: function (store, type, record) {
+    return Ember.RSVP.resolve();
+  },
+
+  generateIdForRecord: function () {
+    return Math.random().toString(32).slice(2).substr(0, 5);
+  }
 });
